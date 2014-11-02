8aeecb89262ea48dbf2565fe928cb7c3884d6d3e
Luke Stanbery <lukestanbery89@gmail.com>
2014-11-02 15:46:58 -0600

    Program complete.

--- ASCII Tree Generator/Program.cs
+++ ASCII Tree Generator/Program.cs
@@ -31,10 +31,29 @@ namespace ASCII_Tree_Generator {
                 char leafChar = Convert.ToChar(parts[1]);
                 char baseChar = Convert.ToChar(parts[2]);
 
-                
+                // Print tree leaves
+                for(uint i = 1; i <= treeWidth; i += 2) {
+                    PrintSpaces((int)((treeWidth - i) / 2));
+                    for(int j = 0; j < i; j++) {
+                        Console.Write(leafChar);
+                    }
+                    Console.Write("\n");
+                }
+
+                // Print tree base
+                PrintSpaces((treeWidth - 2) / 2);
+                Console.WriteLine("{0}{1}{2}", baseChar, baseChar, baseChar);
+
+                Console.WriteLine();
             } catch(Exception ex) { // Pokemon exception handling
                 Console.WriteLine("!!! Invalid input !!!");
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void PrintSpaces(int x) {
+            for(int i = x; i != 0; i--) {
+                Console.Write(" ");
             }
         }
     }
